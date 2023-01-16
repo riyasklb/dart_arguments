38b07bc81f762b987739650aca086b9738cbc6d4
riyasklb <riyasklb89@gmail.com>
2023-01-16 12:04:57 +0530

    first commit

---
+++ lib/bin/dart_application_16.dart
@@ -0,0 +1,10 @@
+//import 'package:dart_application_16/dart_application_16.dart'
+// as dart_application_16;
+
+void main(List<String> arguments) {
+  int a = 3;
+  int b = 2;
+  print(a & b);
+  print(a | b);
+  print(a ^ b);
+}
